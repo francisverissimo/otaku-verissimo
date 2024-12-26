c770e1d733ad199891eb96a55f8f14357c74475c
francissverissimo <francissv97@gmail.com>
2024-12-25 22:00:13 -0400

    feat(styles): add new colors to Tailwind and apply to css

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,10 @@ module.exports = {
       colors: {
         main: '#FF5F00',
         second: '#01a2e3',
+        'darkBG': '#18181b',
+        'darkTxt': '#fafafa',
+        'lightBG': '#e4e4e7',
+        'lightTxt': '#27272a',
       },
       fontFamily: {
         main: ['Open Sans', 'sans-serif'],
